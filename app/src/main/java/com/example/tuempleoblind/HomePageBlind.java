970543bd9b620b376b8dee21b56a035f84a750c0
pandaoscar <81119860+pandaoscar@users.noreply.github.com>
2024-01-26 20:19:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tuempleoblind/HomePageBlind.java
@@ -0,0 +1,14 @@
+package com.example.tuempleoblind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageBlind extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_blind);
+    }
+}
---
+++ app/src/main/java/com/example/tuempleoblind/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.example.tuempleoblind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+    LottieAnimationView lottie;
+    TextView tx_animation1;
+    TextView tx_animation2;
+    TextView tx_animation3;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        lottie=findViewById(R.id.lottie);
+        lottie.animate().rotation(360).setDuration(500).setStartDelay(900)
+                .withEndAction(() -> lottie.animate().translationY(-2000).setDuration(500).setStartDelay(500));
+        lottie.animate().scaleX(0.5f).scaleY(0.5f).setDuration(500).setStartDelay(900);
+
+        tx_animation1=findViewById(R.id.text_animation1);
+        tx_animation1.animate().translationY(-2000).setDuration(500).setStartDelay(1900);
+
+        tx_animation2=findViewById(R.id.text_animation2);
+        tx_animation2.animate().translationY(2000).setDuration(500).setStartDelay(1900);
+
+        tx_animation3=findViewById(R.id.text_animation3);
+        tx_animation3.animate().translationY(2000).setDuration(500).setStartDelay(1900);
+
+
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 2500);
+    }
+}
