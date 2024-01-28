68aec7110f1bf2d1cbfbbbc0c4dc99ea98956d33
Javy018000 <javiraza7@gmail.com>
2024-01-28 13:19:10 -0500

    Conexión a firebase asegurada, ya se insertan datos del login a firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+
 android {
     namespace = "com.example.tuempleoblind"
     compileSdk = 34
@@ -34,14 +35,17 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-analytics")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.airbnb.android:lottie:3.7.0")
 
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
-    implementation("com.google.firebase:firebase-analytics")
+
 }
--- app/src/main/java/com/example/tuempleoblind/SignUpBlind.java
+++ app/src/main/java/com/example/tuempleoblind/SignUpBlind.java
@@ -1,20 +1,81 @@
 package com.example.tuempleoblind;
 
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpBlind extends AppCompatActivity {
 
-    Button btn;
+    Button btnSignUpSend;
+    EditText campTextSignUpUsername, campTextPasswordSignUp1, campTextPasswordSignUp2;
+    private FirebaseFirestore mfirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_blind);
 
+        mfirestore = FirebaseFirestore.getInstance();
+
+        btnSignUpSend = findViewById(R.id.buttonSignUpSend);
+        campTextSignUpUsername = findViewById(R.id.editTextSignUpUsername);
+        campTextPasswordSignUp1 = findViewById(R.id.editTextPasswordSignUp1);
+        campTextPasswordSignUp2 = findViewById(R.id.editTextPasswordSignUp2);
+
+        btnSignUpSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = campTextSignUpUsername.getText().toString();
+                String password1 = campTextPasswordSignUp1.getText().toString();
+                String password2 = campTextPasswordSignUp2.getText().toString();
+
+                if(username.isEmpty() && password1.isEmpty() && password2.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Completa todos los datos correspondientes", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(!password1.equals(password2)){
+                        Toast.makeText(getApplicationContext(), "Las contraseñas deben coincidir, porfavor verifica nuevamente", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        postUsernameBlind(username,password1);
+                    }
+                }
+            }
+        });
 
 
+    }
+
+    private void postUsernameBlind(String username, String password1) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("Usuario", username);
+        map.put("Contraeña", password1);
+        mfirestore.collection("UsernameBlind").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Registro exitoso", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Error al ingresar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
