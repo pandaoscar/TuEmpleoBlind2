17a4e9a8ca93c101b69c5ec90da70291174abbff
pandaoscar <81119860+pandaoscar@users.noreply.github.com>
2024-01-28 13:50:51 -0500

    First Background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,5 +39,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.airbnb.android:lottie:3.7.0")
+    implementation("com.airbnb.android:lottie:6.3.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
 }
