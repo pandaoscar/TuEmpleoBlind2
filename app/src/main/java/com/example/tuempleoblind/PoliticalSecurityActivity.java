2c5da20f0bca8f6067db50a80f844c805af2dbd5
pandaoscar <81119860+pandaoscar@users.noreply.github.com>
2024-05-03 12:13:40 -0500

    Incorporacion politicas de seguridad

--- app/src/main/java/com/example/tuempleoblind/PoliticalSecurityActivity.java
+++ app/src/main/java/com/example/tuempleoblind/PoliticalSecurityActivity.java
@@ -2,25 +2,22 @@ package com.example.tuempleoblind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class AboutActivity extends AppCompatActivity {
+public class PoliticalSecurityActivity extends AppCompatActivity {
     Button btnclose;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        btnclose=findViewById(R.id.buttonCloseAbout);
+        setContentView(R.layout.activity_political_security);
+        btnclose=findViewById(R.id.buttonCloseSecurity);
         btnclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            finish();
+                finish();
             }
         });
     }
